1f81ea2a3a68c38b93af7ef3ef0fb34d9eac8c4e
Eric Thor <me@ericthor.com>
2014-06-24 09:49:34 -0400

    Deleted Bootstrap and updated scripts

--- lib/scripts.php
+++ lib/scripts.php
@@ -11,7 +11,7 @@
  * 3. /theme/assets/js/main.min.js (in footer)
  */
 function roots_scripts() {
-  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, '335476ad8240c44a0e4da145db39f80f');
+  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, 'a13ade08acc83870d3efb3f45205a657');
 
   // jQuery is loaded using the same method from HTML5 Boilerplate:
   // Grab Google CDN's latest jQuery with a protocol relative URL; fallback to local if offline
